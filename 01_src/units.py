16e16c916dfd54c2177214188236f4df92d19e66
fsteinha <franksteinhans@yahoo.com>
2022-05-31 18:08:50 +0200

    add gitignore

--- 01_src/units.py
+++ 01_src/units.py
@@ -1,26 +1,38 @@
-class CType:
-    type_TERRITORY = 0
-    type_UNIT      = 1
-    type_IPC       = 2
-    type_END       = 3
-
-    def get_type(self, t_type:int) -> str:
-        
-        if t_type >= self.type_TERRITORY:
-            return "Territory"
-
-        if t_type >= self.type_UNIT:
-            return "Unit"
-
-        if t_type >= self.type_IPC:
-            return "IPC"
-        
-        return None
-        
 class CType_Territory:
     type_TERRITORY_LAND = 0
     type_TERRITORY_SEA  = 1
     type_TERRITORY_END  = 2
+    
+    d_Str = {
+        type_TERRITORY_LAND : "Land",
+        type_TERRITORY_SEA  : "Sea",
+        type_TERRITORY_END  :  None
+    }
+
+    def get_type(self, t_type:int) -> str:
+        try:
+            return self.d_Str[t_type]
+        except:
+            return None
+    
+class CType_Unit:
+    type_Unit_Infantary          = 0
+    type_Unit_MechInfantary      = 1
+    type_Unit_Tank               = 2
+    type_Unit_Artillery          = 3
+    type_Unit_AAA                = 4
+    type_Unit_Fighter            = 5
+    type_Unit_TacticalBomber     = 6
+    type_Unit_StraticalBomer     = 7
+    type_Unit_Cargo              = 8
+    type_Unit_Submarine          = 9
+    type_Unit_Destroyer          = 10
+    type_Unit_Cruiser            = 11
+    type_Unit_BattleShip         = 12
+    type_Unit_Carrier            = 13
+    
+    
+    
     
     def get_type(self, t_type:int) -> str:
         
@@ -33,8 +45,43 @@ class CType_Territory:
         return None
     
 
+
+class CType:
+    type_TERRITORY = 0
+    type_UNIT      = 1
+    type_IPC       = 2
+    type_END       = 3
+    
+    @staticmethod
+    def get_type(t_type:int) -> str:
+        
+        if t_type >= CType.type_TERRITORY:
+            return "Territory"
+
+        if t_type >= CType.type_UNIT:
+            return "Unit"
+
+        if t_type >= CType.type_IPC:
+            return "IPC"
+        
+        return None
+    
+    @staticmethod
+    def get_sub_type(t_type:int, t_subtype:int) -> str:
+        if t_type >= CType.type_TERRITORY:
+            return CType_Territory.get_type(t_subtype)
+
+        if t_type >= CType.type_UNIT:
+            return "Unit"
+
+        if t_type >= CType.type_IPC:
+            return "IPC"
+        
+        return None
+        
+
 class CItem:
-    def __init__(self, s_name:str, t_type:CType) -> None:
+    def __init__(self, s_name:str, t_type:CType, t_subtype) -> None:
         self.s_name = s_name
 
         if CType.get_type(t_type) != None:
@@ -44,4 +91,9 @@ class CItem:
 
         pass
 
-class CTerritory
+
+# test 
+if __name__ == "__main__":
+    print (f"{CType.type_TERRITORY}")
+    print (f"{CType.get_type(CType.type_TERRITORY)}")
+    
