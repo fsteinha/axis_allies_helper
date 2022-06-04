f85a3fe397955879f481f0e63ce72b79863a22d8
fsteinha <franksteinhans@yahoo.com>
2022-06-04 12:47:04 +0200

    add selfcheck

--- 01_src/type.py
+++ 01_src/type.py
@@ -23,50 +23,50 @@ class CType():
     U_BATTLESHIP         = 112
     U_CARRIER            = 113
 
-    STR_C_TERRITORY = "Territory"
-    STR_C_UNIT      = "Unit"
-    STR_C_IPC      = "IPC"
+    STR_C_TERRITORY       = "Territory"
+    STR_C_UNIT            = "Unit"
+    STR_C_IPC             = "IPC"
 
-    STR_T_LAND = "Land"
-    STR_T_SEA  = "Sea"
+    STR_T_LAND            = "Land"
+    STR_T_SEA             = "Sea"
 
-    STR_U_INFANTARY          = "Infantary"
-    STR_U_MECH_INFANTARY     = "Mech-Infantary"
-    STR_U_TANK               = "U_TANK"
-    STR_U_ARTILLERY          = "U_ARTILLERY"
-    STR_U_AAA                = "U_AAA"
-    STR_U_FIGHTER            = "U_FIGHTER"
-    STR_U_T_BOMBER           = "Tactical-Bomber"
-    STR_U_S_BOMBER           = "Stratical-Bomber"
-    STR_U_CARGO              = "U_CARGO"
-    STR_U_SUBMARINE          = "U_SUBMARINE"
-    STR_U_DESTROYER          = "U_DESTROYER"
-    STR_U_CRUISER            = "U_CRUISER"
-    STR_U_BATTLESHIP         = "Battelship"
-    STR_U_CARRIER            = "U_CARRIER"
+    STR_U_INFANTARY       = "Infantary"
+    STR_U_MECH_INFANTARY  = "Mech-Infantary"
+    STR_U_TANK            = "Tank"
+    STR_U_ARTILLERY       = "Artillery"
+    STR_U_AAA             = "AAA"
+    STR_U_FIGHTER         = "Fighter"
+    STR_U_T_BOMBER        = "Tactical-Bomber"
+    STR_U_S_BOMBER        = "Stratical-Bomber"
+    STR_U_CARGO           = "Cargo"
+    STR_U_SUBMARINE       = "Submarine"
+    STR_U_DESTROYER       = "Destroyer"
+    STR_U_CRUISER         = "Cruier"
+    STR_U_BATTLESHIP      = "Battelship"
+    STR_U_CARRIER         = "Carrier"
     
     d_Str = {
-        C_TERRITORY          : STR_C_TERRITORY,
-        C_UNIT               : STR_C_UNIT,
-        C_IPC                : STR_C_IPC,        
+        C_TERRITORY     : STR_C_TERRITORY,
+        C_UNIT          : STR_C_UNIT,
+        C_IPC           : STR_C_IPC,        
 
-        T_LAND               : STR_T_LAND,
-        T_SEA                : STR_T_SEA,
+        T_LAND          : STR_T_LAND,
+        T_SEA           : STR_T_SEA,
         
-        U_INFANTARY          : STR_U_INFANTARY,
-        U_MECH_INFANTARY     : STR_U_MECH_INFANTARY,
-        U_TANK               : STR_U_TANK,
-        U_ARTILLERY          : STR_U_ARTILLERY,
-        U_AAA                : STR_U_AAA,
-        U_FIGHTER            : STR_U_FIGHTER,
-        U_T_BOMBER           : STR_U_T_BOMBER,
-        U_S_BOMBER           : STR_U_S_BOMBER,
-        U_CARGO              : STR_U_CARGO,
-        U_SUBMARINE          : STR_U_SUBMARINE,
-        U_DESTROYER          : STR_U_DESTROYER,
-        U_CRUISER            : STR_U_CRUISER,
-        U_BATTLESHIP         : STR_U_BATTLESHIP,
-        U_CARRIER            : STR_U_CARRIER
+        U_INFANTARY     : STR_U_INFANTARY,
+        U_MECH_INFANTARY: STR_U_MECH_INFANTARY,
+        U_TANK          : STR_U_TANK,
+        U_ARTILLERY     : STR_U_ARTILLERY,
+        U_AAA           : STR_U_AAA,
+        U_FIGHTER       : STR_U_FIGHTER,
+        U_T_BOMBER      : STR_U_T_BOMBER,
+        U_S_BOMBER      : STR_U_S_BOMBER,
+        U_CARGO         : STR_U_CARGO,
+        U_SUBMARINE     : STR_U_SUBMARINE,
+        U_DESTROYER     : STR_U_DESTROYER,
+        U_CRUISER       : STR_U_CRUISER,
+        U_BATTLESHIP    : STR_U_BATTLESHIP,
+        U_CARRIER       : STR_U_CARRIER
     }
     
     @staticmethod
@@ -74,25 +74,31 @@ class CType():
         try:
             return CType.d_Str[t_type]
         except:
-            return None
-    
-class CTClass():
-    
-    
-    d_Str = {
-    }
-
-    @staticmethod
-    def get_str(t_type:int) -> str:
-        try:
-            return CTClass.d_Str[t_type]
-        except:
-            return None
-        
+            return None      
 
 # test 
 if __name__ == "__main__":
-    print (f"{CTClass.TERRITORY}")
-    print (f"{CTClass.get_str(CTClass.TERRITORY)}")
-    print (f"{CTTerritory.get_str(CTTerritory.TERRITORY_LAND)}")
+    assert (CType.get_str(CType.C_TERRITORY)      == "Territory")
+    assert (CType.get_str(CType.C_UNIT)           == "Unit")
+    assert (CType.get_str(CType.C_IPC)            == "IPC")
+
+    assert (CType.get_str(CType.T_LAND)           == "Land")
+    assert (CType.get_str(CType.T_SEA)            == "Sea")
+    
+    assert (CType.get_str(CType.U_INFANTARY)      == "Infantary")
+    assert (CType.get_str(CType.U_MECH_INFANTARY) == "Mech-Infantary")
+    assert (CType.get_str(CType.U_TANK)           == "Tank")
+    assert (CType.get_str(CType.U_ARTILLERY)      == "Artillery")
+    assert (CType.get_str(CType.U_AAA)            == "AAA")
+    assert (CType.get_str(CType.U_FIGHTER)        == "Fighter")
+    assert (CType.get_str(CType.U_T_BOMBER)       == "Tactical-Bomber")
+    assert (CType.get_str(CType.U_S_BOMBER)       == "Stratical-Bomber")
+    assert (CType.get_str(CType.U_CARGO)          == "Cargo")
+    assert (CType.get_str(CType.U_SUBMARINE)      == "Submarine")
+    assert (CType.get_str(CType.U_DESTROYER)      == "Destroyer")
+    assert (CType.get_str(CType.U_CRUISER)        == "Cruier")
+    assert (CType.get_str(CType.U_BATTLESHIP)     == "Battelship")
+    assert (CType.get_str(CType.U_CARRIER)        == "Carrier")
+
+
     
