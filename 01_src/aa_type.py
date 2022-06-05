8e369a4bace4ad7b2ba6ec561d06fa2c03e4e4ba
fsteinha <franksteinhans@yahoo.com>
2022-06-05 09:25:10 +0200

    add Items and tests for nation and land

--- 01_src/aa_type.py
+++ 01_src/aa_type.py
@@ -4,6 +4,9 @@ class CType():
     C_TERRITORY         = 0
     C_UNIT              = 1
     C_IPC               = 2
+    C_NATION            = 3
+    C_ALLIANCE          = 4
+    C_FACILITY          = 5
 
     T_LAND              = 10
     T_SEA               = 11
@@ -22,83 +25,94 @@ class CType():
     U_CRUISER            = 111
     U_BATTLESHIP         = 112
     U_CARRIER            = 113
-
-    STR_C_TERRITORY       = "Territory"
-    STR_C_UNIT            = "Unit"
-    STR_C_IPC             = "IPC"
-
-    STR_T_LAND            = "Land"
-    STR_T_SEA             = "Sea"
-
-    STR_U_INFANTARY       = "Infantary"
-    STR_U_MECH_INFANTARY  = "Mech-Infantary"
-    STR_U_TANK            = "Tank"
-    STR_U_ARTILLERY       = "Artillery"
-    STR_U_AAA             = "AAA"
-    STR_U_FIGHTER         = "Fighter"
-    STR_U_T_BOMBER        = "Tactical-Bomber"
-    STR_U_S_BOMBER        = "Stratical-Bomber"
-    STR_U_CARGO           = "Cargo"
-    STR_U_SUBMARINE       = "Submarine"
-    STR_U_DESTROYER       = "Destroyer"
-    STR_U_CRUISER         = "Cruier"
-    STR_U_BATTLESHIP      = "Battelship"
-    STR_U_CARRIER         = "Carrier"
     
-    d_Str = {
-        C_TERRITORY     : STR_C_TERRITORY,
-        C_UNIT          : STR_C_UNIT,
-        C_IPC           : STR_C_IPC,        
+    A_AXIS               = 200
+    A_ALLIES             = 201
 
-        T_LAND          : STR_T_LAND,
-        T_SEA           : STR_T_SEA,
+    F_MAJOR_FACTORY     = 300
+    F_MINOR_FACTORY     = 301
+    F_AIR_BASE          = 302
+    F_NAVAL_BASE        = 303
+    
+    NA                  = 1000    
+      
+    d_Str = {
+        C_TERRITORY     : "Territory",
+        C_UNIT          : "Unit",
+        C_IPC           : "IPC",
+        C_NATION        : "Nation",
+        C_ALLIANCE      : "Alliance",
+        C_FACILITY      : "Facility",
+
+
+        T_LAND          : "Land",
+        T_SEA           : "Sea",
         
-        U_INFANTARY     : STR_U_INFANTARY,
-        U_MECH_INFANTARY: STR_U_MECH_INFANTARY,
-        U_TANK          : STR_U_TANK,
-        U_ARTILLERY     : STR_U_ARTILLERY,
-        U_AAA           : STR_U_AAA,
-        U_FIGHTER       : STR_U_FIGHTER,
-        U_T_BOMBER      : STR_U_T_BOMBER,
-        U_S_BOMBER      : STR_U_S_BOMBER,
-        U_CARGO         : STR_U_CARGO,
-        U_SUBMARINE     : STR_U_SUBMARINE,
-        U_DESTROYER     : STR_U_DESTROYER,
-        U_CRUISER       : STR_U_CRUISER,
-        U_BATTLESHIP    : STR_U_BATTLESHIP,
-        U_CARRIER       : STR_U_CARRIER
+        U_INFANTARY     : "Infantary",
+        U_MECH_INFANTARY: "Mech-Infantary",
+        U_TANK          : "Tank",
+        U_ARTILLERY     : "Artillery",
+        U_AAA           : "AAA",
+        U_FIGHTER       : "Fighter",
+        U_T_BOMBER      : "Tactical-Bomber",
+        U_S_BOMBER      : "Stratic-Bomber",
+        U_CARGO         : "Cargo",
+        U_SUBMARINE     : "Submarine",
+        U_DESTROYER     : "Destroyer",
+        U_CRUISER       : "Cruier",
+        U_BATTLESHIP    : "Battelship",
+        U_CARRIER       : "Carrier",
+
+        A_AXIS          : "Axis",
+        A_ALLIES        : "Allies",
+
+        F_MAJOR_FACTORY : "Major factory",
+        F_MINOR_FACTORY : "Minor factory",
+        F_AIR_BASE      : "Air base",
+        F_NAVAL_BASE    : "Naval base",
+
+        NA              : "N/A"
     }
     
     @staticmethod
-    def get_str(t_type:int) -> str:
+    def str(t_type:int) -> str:
         try:
             return CType.d_Str[t_type]
         except:
-            return None      
+            return None   
 
+    @staticmethod
+    def type(s_type:str) -> int:
+        keys = [k for k, v in CType.d_Str.items() if v == s_type]
+        return keys[0]
+
+    @staticmethod
+    def na()->str:
+        return CType.str(CType.NA)    
 # test 
 if __name__ == "__main__":
-    assert (CType.get_str(CType.C_TERRITORY)      == "Territory")
-    assert (CType.get_str(CType.C_UNIT)           == "Unit")
-    assert (CType.get_str(CType.C_IPC)            == "IPC")
+    assert (CType.str(CType.C_TERRITORY)      == "Territory")
+    assert (CType.str(CType.C_UNIT)           == "Unit")
+    assert (CType.str(CType.C_IPC)            == "IPC")
+    assert (CType.str(CType.C_NATION)         == "Nation")
 
-    assert (CType.get_str(CType.T_LAND)           == "Land")
-    assert (CType.get_str(CType.T_SEA)            == "Sea")
+    assert (CType.str(CType.T_LAND)           == "Land")
+    assert (CType.str(CType.T_SEA)            == "Sea")
     
-    assert (CType.get_str(CType.U_INFANTARY)      == "Infantary")
-    assert (CType.get_str(CType.U_MECH_INFANTARY) == "Mech-Infantary")
-    assert (CType.get_str(CType.U_TANK)           == "Tank")
-    assert (CType.get_str(CType.U_ARTILLERY)      == "Artillery")
-    assert (CType.get_str(CType.U_AAA)            == "AAA")
-    assert (CType.get_str(CType.U_FIGHTER)        == "Fighter")
-    assert (CType.get_str(CType.U_T_BOMBER)       == "Tactical-Bomber")
-    assert (CType.get_str(CType.U_S_BOMBER)       == "Stratical-Bomber")
-    assert (CType.get_str(CType.U_CARGO)          == "Cargo")
-    assert (CType.get_str(CType.U_SUBMARINE)      == "Submarine")
-    assert (CType.get_str(CType.U_DESTROYER)      == "Destroyer")
-    assert (CType.get_str(CType.U_CRUISER)        == "Cruier")
-    assert (CType.get_str(CType.U_BATTLESHIP)     == "Battelship")
-    assert (CType.get_str(CType.U_CARRIER)        == "Carrier")
+    assert (CType.str(CType.U_INFANTARY)      == "Infantary")
+    assert (CType.str(CType.U_MECH_INFANTARY) == "Mech-Infantary")
+    assert (CType.str(CType.U_TANK)           == "Tank")
+    assert (CType.str(CType.U_ARTILLERY)      == "Artillery")
+    assert (CType.str(CType.U_AAA)            == "AAA")
+    assert (CType.str(CType.U_FIGHTER)        == "Fighter")
+    assert (CType.str(CType.U_T_BOMBER)       == "Tactical-Bomber")
+    assert (CType.str(CType.U_S_BOMBER)       == "Stratical-Bomber")
+    assert (CType.str(CType.U_CARGO)          == "Cargo")
+    assert (CType.str(CType.U_SUBMARINE)      == "Submarine")
+    assert (CType.str(CType.U_DESTROYER)      == "Destroyer")
+    assert (CType.str(CType.U_CRUISER)        == "Cruier")
+    assert (CType.str(CType.U_BATTLESHIP)     == "Battelship")
+    assert (CType.str(CType.U_CARRIER)        == "Carrier")
 
 
     
--- 01_src/units.py
+++ 01_src/units.py
@@ -1,25 +1,205 @@
+import string
 from  aa_type import CType
 
+##############################################################################
 class CAAItem():
-    def __init__(self,s_name) -> None:
+    def __init__(self,s_name, aa_type) -> None:
         self.s_name = s_name
 
-    def set_type(self,aa_type) -> bool:    
         if type(aa_type) == str:
-            self.aa_type = CType.get_type(aa_type)
-        elif  CType.get_str(aa_type) != None:
+            self.aa_type = CType.type(aa_type)
+        elif  CType.str(aa_type) != None:
             self.aa_type = aa_type
-        
-        return self.aa_type != None
 
-    def print(self):
-        s_out = f'''
-Name: {self.s_name}
-Type: {CType.get_str(self.aa_type)}
-                 '''
-        print (s_out)
+    def get_name(self) -> str:
+        return self.s_name
+
+    def get_type(self) -> str:
+        return CType.str(self.aa_type)
+
+    def get_nation(self) -> str:
+        if ('aa_nation' in dir(self)):
+            return self.aa_nation.s_name
+        return CType.na()
+
+    def get_alliance(self) -> str:
+        if ('aa_alliance' in dir(self)):
+            return CType.str(self.aa_alliance)
+
+        return self.aa_nation.get_alliance()
+
+    def get_ipc(self) -> int:
+        if ('aa_ipc' in dir(self)):
+            return self.aa_ipc
+        return CType.na()
+
+    def get_units(self) -> int:
+        if ('l_aa_units' in dir(self)):
+            return str(self.l_aa_units)
+        return CType.na()
+
+
+    def info(self):
+        return f'''
+Name    : {self.get_name()}
+Type    : {self.get_type()}
+Nation  : {self.get_nation()}
+Alliance: {self.get_alliance()}
+IPC     : {self.get_ipc()}
+Units   : {self.get_units()}
+'''
+
+##############################################################################
+class CAANation(CAAItem):
+    def __init__(self, s_name:str, alliance:int) -> None:
+        super().__init__(s_name, CType.C_NATION)
+        self.aa_alliance = alliance
+        pass
+
+
+##############################################################################
+class CAALand(CAAItem):
+    def __init__(self, s_name:str, aa_nation:CAANation, aa_ipc:int, l_aa_units:list = []) -> None:
+        super().__init__(s_name, CType.T_LAND)
+        self.aa_ipc=aa_ipc
+        self.l_aa_units = l_aa_units
+        self.aa_nation = aa_nation
+        pass
         
+##############################################################################
+class CAASea(CAAItem):
+    def __init__(self, s_name:str, aa_ipc:int, l_aa_units:list = []) -> None:
+        super().__init__(s_name, CType.T_SEA)
+        self.l_aa_units = l_aa_units
+        pass
+
+
+##############################################################################
+class CAAUnit(CAAItem):
+    def __init__(self, aa_type:int, aa_nation, i_count = 1) -> None:
+        super().__init__(CType.str(aa_type), aa_type)
+        self.set_nation(aa_nation)
+        self.i_count = i_count
+        pass
+
+##############################################################################
+class CAAInf(CAAUnit):
+    def __init__(self, aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_INFANTARY, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAAMechInf(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_MECH_INFANTARY, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAATank(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_TANK, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAAAri(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_ARTILLERY, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAAAAA(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_AAA, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAAFigther(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_FIGHTER, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAATBomb(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_T_BOMBER, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAASBomb(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_S_BOMBER, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAASubmarine(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_SUBMARINE, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAADestroyer(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_DESTROYER, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAACruiser(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_CRUISER, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAABattleship(CAAUnit):
+    def __init__(self,  aa_nation, i_count = 1) -> None:
+        super().__init__(CType.U_BATTLESHIP, aa_nation, i_count)
+        pass
+
+##############################################################################
+class CAACarrier(CAAUnit):
+    def __init__(self,  aa_nation, i_count:int = 1, l_aa_units:list = None) -> None:
+        super().__init__(CType.U_CARRIER, aa_nation, i_count)
+        self.l_aa_units = l_aa_units
+        pass
+
+##############################################################################
+class CAACargo(CAAUnit):
+    def __init__(self,  aa_nation, i_count:int = 1, l_aa_units:list = None) -> None:
+        super().__init__(CType.U_CARGO, aa_nation, i_count)
+        self.l_aa_units = l_aa_units
+        pass
+
+##############################################################################
+def test_nation(s_name:string, aa_alliance:int):
+    caa = CAANation(s_name, aa_alliance)
+    assert(caa.get_name() == s_name)
+    assert(caa.get_type() == CType.str(CType.C_NATION))
+    assert(caa.get_alliance() == CType.str(aa_alliance))
+    print(caa.info())    
+
+def test_land(s_name:string, aa_nation:CAANation, aa_ipc:int, l_aa_units:list):
+    caa = CAALand(s_name, aa_nation, aa_ipc, l_aa_units)
+    assert(caa.get_name() == s_name)
+    assert(caa.get_type() == CType.str(CType.T_LAND))
+    assert caa.get_alliance() == aa_nation.get_alliance(), f"{caa.get_alliance()} != {aa_nation.get_alliance()}"
+    assert(caa.get_ipc()  == aa_ipc)
+    assert(caa.get_units()  == str(l_aa_units))
+    
+    #TBD list
+    print(caa.info())
+    
+    
+##############################################################################
 if __name__ == "__main__":
-    item = CAAItem("test")
-    item.set_type(CType.T_LAND)
-    item.print()
+    test_nation("Germany"              , CType.A_AXIS)
+    test_nation("Sovietunion"          , CType.A_ALLIES)
+    test_nation("Japan"                , CType.A_AXIS)
+    test_nation("United States"        , CType.A_ALLIES)
+    test_nation("Italy"                , CType.A_AXIS)
+    test_nation("Great Britain Europe" , CType.A_ALLIES)
+    test_nation("Great Britain Pacific", CType.A_ALLIES)
+    test_nation("France"               , CType.A_ALLIES)
+    test_nation("China"                , CType.A_ALLIES)
+    test_nation("ANZAC"                , CType.A_ALLIES)
+    test_land("Western Germany",CAANation("Germany",CType.A_AXIS),10,[])
+    
+    
