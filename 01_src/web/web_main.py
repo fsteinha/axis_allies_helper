2ab32d118ffffe845a3f58fd6b942f8579e2f410
fsteinha <franksteinhans@yahoo.com>
2022-09-12 21:50:22 +0200

    Save works but not the loading
    Changes to be committed:
    	new file:   sessions/New_standard_session_Global_1940.aa
    	modified:   static/css/style.css
    	modified:   templates/base.html
    	new file:   templates/info.html
    	modified:   templates/me_save.html
    	modified:   templates/session_status.html
    	modified:   web_main.py

--- 01_src/web/web_main.py
+++ 01_src/web/web_main.py
@@ -27,7 +27,7 @@ from werkzeug.exceptions import abort
 # Loaded session
 
 S_STD_SESSION_NAME = "New standard session (Global 1940)"
-
+S_SESSION_PATH = "./sessions"
 
 # FLASK app 
 app = Flask(__name__)
@@ -44,12 +44,12 @@ Session(app)
 
 @app.route('/')
 def me_status():
-    aa_session = get_web_session()
+    web_session = get_web_session()
     #print (session['S_CURRENT_SESSION_NAME']) 
-    if aa_session.AA_SESSION == None:
-        return render_template('index.html', header=get_aa_session_status(), info=" !!! No session loaded !!!")
+    if web_session.AA_SESSION == None:
+        return render_template('index.html', header=get_web_session_status(), info=" !!! No session loaded !!!")
 
-    return render_template('session_status.html', header=get_aa_session_status(), aa_session=aa_session.AA_SESSION)
+    return render_template('session_status.html', header=get_web_session_status(), aa_session=web_session.AA_SESSION)
 
 @app.route('/me_load')
 def me_load():
@@ -58,21 +58,44 @@ def me_load():
 
 @app.route('/me_save')
 def me_save():
-    return render_template('me_save.html', header="Save", info="me_2" )
+    web_session = get_web_session()
+    if web_session.AA_SESSION == None:
+        return redirect(url_for('me_status'))
+
+    if web_session.S_CURRENT_SESSION_NAME == S_STD_SESSION_NAME:
+        s_save_session_name = S_STD_SESSION_NAME.replace(' ', '_').replace('(', '').replace(')','') + ".aa"
+
+    return render_template('me_save.html', header="Save", s_save_name=s_save_session_name )
+
+@app.route('/me_save', methods=['POST'])
+def me_save_session():
+    web_session = get_web_session()
+
+    if web_session.AA_SESSION == None:
+        return redirect(url_for('me_status'))
+
+    s_file_name = request.form['save_file']
+
+    CFILE_Session(S_SESSION_PATH + "/" + s_file_name).save_session(web_session.AA_SESSION)
+    
+    s_info = s_file_name + " saved"
+
+    return render_template('info.html', header="Save status", info=s_info)
+
 
 @app.route('/me_3')
 def me_3():
     return render_template('me_3.html', header="me_3", info="me_3" )
-
+        
 @app.route('/me_4')
 def me_4():
     return render_template('me_4.html', header="me_4", info="me_4" )
 
 @app.route('/<string:session_str>')
 def aa_session(session_str):
-    aa_session = get_web_session()
-    aa_session.AA_SESSION = get_aa_session(session_str)
-    aa_session.S_CURRENT_SESSION_NAME = session_str
+    web_session = get_web_session()
+    web_session.AA_SESSION = get_aa_session(session_str)
+    web_session.S_CURRENT_SESSION_NAME = session_str
 
     return redirect(url_for('me_status'))
 
@@ -80,14 +103,15 @@ def aa_session(session_str):
 # local/helper functions
 ##############################################################################
 def read_aa_sessions() -> list:
-    l_sessions = glob.glob("./sessions/*.aa")
+    l_sessions = glob.glob(S_SESSION_PATH + "/*.aa")
+
     l_sessions.append(S_STD_SESSION_NAME)
     return l_sessions
 
 def get_aa_session(session):
     aa_session = None
     if (session != S_STD_SESSION_NAME):
-        aa_session = CFILE.load_session(session)
+        aa_session = CFILE_Session.load_session(session)
     else:
         aa_session = CAAI_Session(s_name            = "Global game 1940",
                                   aa_map            = C_MAP_GLOBAL_1940,
@@ -97,13 +121,13 @@ def get_aa_session(session):
                                   aa_current_phase  = CType.S_PH1_PURCHASE_REPAIR)
     return aa_session
 
-def get_aa_session_status():
+def get_web_session_status():
     s_status = "Status"
-    aa_session = get_web_session()
+    web_session = get_web_session()
     
-    if (aa_session.S_CURRENT_SESSION_NAME == None):
+    if (web_session.S_CURRENT_SESSION_NAME == None):
         s_status = s_status + ": No session"
     else:
-        s_status = s_status + f": {aa_session.S_CURRENT_SESSION_NAME}"
+        s_status = s_status + f": {web_session.S_CURRENT_SESSION_NAME}"
 
     return s_status
